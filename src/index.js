d4b7fbc4425083bc6e9afeb9155bfa2bd21ed127
Zayd <zdixon008@gmail.com>
2022-09-28 09:19:06 -0700

    Supports multiple channels and file extensions

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 require('dotenv').config();
 const TOKEN = process.env.TOKEN;
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
 
-const { Guilds, GuildMessages, GuildMessageReactions } = GatewayIntentBits;
+const { Guilds, GuildMessages, GuildMessageReactions, MessageContent } = GatewayIntentBits;
 const client = new Client({
     intents: [
         Guilds,
         GuildMessages,
-        GuildMessageReactions
+        GuildMessageReactions,
+        MessageContent
     ]
 });
 
@@ -21,11 +22,19 @@ client.on("messageCreate", (message) => {
     if (message.author.id === "328927740617031691") {
         message.react("<:lolicon:1022021897157869588>");
     }
-    if (message.channelId === "448335060006076416") {
-        message.react("<:upvote:806928734782881842>");
-        message.react("<:downvote:806928697910493224>");
-    }
-
+    const validExtensins = [".png", ".jpg", ".mp4", ".mov", ".webm"];
+    const memeChannals = ["876810952039878728", "950912874266652702", "950912897335328848"];
+    
+    memeChannals.forEach(channel => {
+        if (message.channelId === channel) {
+            validExtensins.forEach(extension => {
+                if (message.attachments.size > 0 || message.content.endsWith(extension)) {
+                    message.react("<:upvote:806928734782881842>");
+                    message.react("<:downvote:806928697910493224>");
+                }
+            })
+        }
+    })
 });
 
 client.login(TOKEN);
