e78cff8d657276ab9142cec61791b458b9affc82
Zayd <zdixon008@gmail.com>
2022-09-30 12:25:50 -0700

    Added command handling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const TOKEN = process.env.TOKEN;
+const fs = require('node:fs');
+const path = require('node:path');
 const { Client, GatewayIntentBits, Collection } = require('discord.js');
 
 const { Guilds, GuildMessages, GuildMessageReactions, MessageContent } = GatewayIntentBits;
@@ -12,8 +14,33 @@ const client = new Client({
     ]
 });
 
+client.commands = new Collection();
+const commandPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandPath).filter(file => file.endsWith('.js'));
+
+for(const file of commandFiles) {
+    const filePath = path.join(commandPath, file);
+    const command = require(commandPath);
+    client.commands.set(command.data.name, command);
+}
+
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on("interactionCreate", async interaction => {
+    if(!interaction.isChatInputCommand()) return;
+
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) return;
+    
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });  
+    }
 });
 
 client.on("messageCreate", (message) => {
