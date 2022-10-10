3b56fa9e30215ce73228ad871794ce46a4871490
Zayd <105383646+zaydjd@users.noreply.github.com>
2022-10-09 18:28:24 -0700

    Initial command creation (again)

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,14 +1,20 @@
-const fs = require('node:fs');
-const path = require('node:path');
-const { REST, Routes } = require('discord.js');
-const { clientId, guildId, token } = require('./config.json');
+import { REST, SlashCommandBuilder, Routes} from 'discord.js';
+import { config } from 'dotenv';
 
-const commands = [];
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+config();
+const TOKEN = process.env.BOT_TOKEN;
+const CLIENT_ID = process.env.BOT_CLIENT_ID;
+const GUILD_ID = process.env.BOT_GUILD_ID;
 
-for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	commands.push(command.data.toJSON());
-}
+const commands = [
+	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
+	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
+	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
+]
+	.map(command => command.toJSON());
+
+const rest = new REST({ version: '10' }).setToken(TOKEN);
+
+rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: commands })
+	.then((data) => console.log(`Successfully registered ${data.length} application commands.`))
+	.catch(console.error);
--- src/index.js
+++ src/index.js
@@ -1,67 +1,59 @@
-require('dotenv').config();
-const TOKEN = process.env.TOKEN;
-const fs = require('node:fs');
-const path = require('node:path');
-const { Client, GatewayIntentBits, Collection } = require('discord.js');
+import { config } from 'dotenv';
+import { Client, GatewayIntentBits, Collection, Guild, Routes } from 'discord.js';
 
-const { Guilds, GuildMessages, GuildMessageReactions, MessageContent } = GatewayIntentBits;
+config();
+
+const TOKEN = process.env.BOT_TOKEN;
+const CLIENT_ID = process.env.BOT_CLIENT_ID;
+const GUILD_ID = process.env.BOT_GUILD_ID;
+
+const { Guilds, GuildMessages, GuildMembers, GuildMessageReactions, MessageContent } = GatewayIntentBits;
+
 const client = new Client({
     intents: [
         Guilds,
         GuildMessages,
+        GuildMembers,
         GuildMessageReactions,
         MessageContent
     ]
 });
 
-client.commands = new Collection();
-const commandPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandPath).filter(file => file.endsWith('.js'));
+client.on("ready", () => console.log(`Logged in as ${client.user.tag}`));
 
-for(const file of commandFiles) {
-    const filePath = path.join(commandPath, file);
-    const command = require(commandPath);
-    client.commands.set(command.data.name, command);
-}
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isChatInputCommand()) return;
 
-client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}`);
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+	} else if (commandName === 'user') {
+		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+	}
 });
 
-client.on("interactionCreate", async interaction => {
-    if(!interaction.isChatInputCommand()) return;
+// client.on("messageCreate", (message) => {
+//     if(message.author.bot) return; 
 
-    const command = interaction.client.commands.get(interaction.commandName);
-
-    if (!command) return;
+//     if (message.author.id === "328927740617031691") {
+//         message.react("<:lolicon:1022021897157869588>");
+//     }
+//     const validExtensins = [".png", ".jpg", ".mp4", ".mov", ".webm"];
+//     const memeChannals = ["876810952039878728", "950912874266652702", "950912897335328848", "448335060006076416"];
     
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        console.error(error);
-        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });  
-    }
-});
-
-client.on("messageCreate", (message) => {
-    if(message.author.bot) return; 
-
-    if (message.author.id === "328927740617031691") {
-        message.react("<:lolicon:1022021897157869588>");
-    }
-    const validExtensins = [".png", ".jpg", ".mp4", ".mov", ".webm"];
-    const memeChannals = ["876810952039878728", "950912874266652702", "950912897335328848"];
-    
-    memeChannals.forEach(channel => {
-        if (message.channelId === channel) {
-            validExtensins.forEach(extension => {
-                if (message.attachments.size > 0 || message.content.endsWith(extension)) {
-                    message.react("<:upvote:806928734782881842>");
-                    message.react("<:downvote:806928697910493224>");
-                }
-            })
-        }
-    })
-});
+//     memeChannals.forEach(channel => {
+//         if (message.channelId === channel) {
+//             validExtensins.forEach(extension => {
+//                 if (message.attachments.size > 0 || message.content.endsWith(extension)) {
+//                     message.react("<:upvote:806928734782881842>");
+//                     message.react("<:downvote:806928697910493224>");
+//                 }
+//             })
+//         }
+//     })
+// });
 
 client.login(TOKEN);
