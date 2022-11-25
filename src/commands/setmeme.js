b506daf977538df8b9d2cda1b9b7f33e71ee5141
Zayd <105383646+zaydjd@users.noreply.github.com>
2022-11-25 11:13:51 -0800

    Command to set meme channels

---
+++ src/commands/setmeme.js
@@ -0,0 +1,41 @@
+const { SlashCommandBuilder } = require("discord.js");
+const fs = require("fs");
+const path = require("path");
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("setmeme")
+		.setDescription("Sets meme channel")
+		.setDefaultMemberPermissions(0),
+	async execute(interaction) {
+		const currentChannel = interaction.channelId;
+
+		let memeChannels = path.join(__dirname, "..\\data\\meme-channels.json");
+		let memeData = JSON.parse(fs.readFileSync(memeChannels).toString());
+
+		let addedChannel = false;
+		let sameGuild = false;
+		memeData.channels.map((element) => {
+			if (element.currentChannel == currentChannel) {
+				addedChannel = true;
+			}
+		});
+
+		const addChannel = () => {
+			let memeChannel = { currentChannel };
+			memeData.channels.push(memeChannel);
+
+			fs.writeFileSync(memeChannels, JSON.stringify(memeData));
+		};
+
+		if (addedChannel) {
+			await interaction.reply(`<#${currentChannel}> is already set!`);
+			return;
+		} else {
+			addChannel();
+			await interaction.reply(
+				"Set meme channel:\n" + `<#${currentChannel}>`
+			);
+		}
+	},
+};
--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,28 +1,36 @@
+const data = require("../data/meme-channels.json");
+
 module.exports = {
-	name: 'messageCreate',
+	name: "messageCreate",
 	execute(message) {
-        const botId = 1023800611889741824;
+		const botId = 1023800611889741824;
 
-		if(message.author.bot) return; 
+		if (message.author.bot) return;
 
-        // if (message.author.id === "328927740617031691") {
-        //     message.react("1029256527254650942");
-        // }
-        const validExtensins = [".png", ".jpg", ".mp4", ".mov", ".webm"];
-        const memeChannals = ["876810952039878728", "950912874266652702", "950912897335328848", "448335060006076416"];
-        
-        memeChannals.forEach(channel => {
-            if (message.channelId === channel) {
-                validExtensins.forEach(extension => {
-                    if (message.attachments.size > 0 || message.content.endsWith(extension)) {
-                        message.react("<:upvote:1029256925340254218>");
-                        message.react("<:downvote:1029256965890789426> ");
-                    }
-                })
-            }
-        })
-        if (message.mentions.has(message.client.user.id)) {
-            message.channel.send(`<@${message.author.id}>, :)`);
-        }	
-    },
+		let memeChannels = data.channels.map((item) => {
+			return item.currentChannel;
+		});
+
+		// if (message.author.id === "328927740617031691") {
+		//     message.react("1029256527254650942");
+		// }
+		const validExtensins = [".png", ".jpg", ".mp4", ".mov", ".webm"];
+
+		memeChannels.forEach((channel) => {
+			if (message.channelId == channel) {
+				validExtensins.forEach((extension) => {
+					if (
+						message.attachments.size > 0 ||
+						message.content.endsWith(extension)
+					) {
+						message.react("<:upvote:1029256925340254218>");
+						message.react("<:downvote:1029256965890789426> ");
+					}
+				});
+			}
+		});
+		if (message.mentions.has(message.client.user.id)) {
+			message.channel.send(`<@${message.author.id}>, 😉`);
+		}
+	},
 };
