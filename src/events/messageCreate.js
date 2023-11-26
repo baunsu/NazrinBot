7c300729b4eed8f469913de49ced6c10aee33075
Zayd <105383646+zaydjd@users.noreply.github.com>
2023-11-26 04:02:24 -0800

    Added reply to certain keywords

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -11,13 +11,6 @@ module.exports = {
 			return item.currentChannel;
 		});
 
-		// if (message.author.id === "328927740617031691") {
-		// 	message.react("1052898566911299674");
-		// }
-		// if (message.author.id === "186588469164900352") {
-		// 	message.react("1029256965890789426");
-		// }
-
 		const validExtensins = [".png", ".jpg", ".mp4", ".mov", ".webm"];
 
 		memeChannels.forEach((channel) => {
@@ -34,7 +27,18 @@ module.exports = {
 			}
 		});
 		if (message.mentions.has(message.client.user.id)) {
-			message.channel.send(`<@${message.author.id}>, 😉`);
+			let die =
+				message.content.toLowerCase().includes("kys") ||
+				message.content.toLowerCase().includes("kill yourself") ||
+				message.content.toLowerCase().includes("die");
+
+			if (die) {
+				message.channel.send(`<@${message.author.id}>, :(`);
+			} else {
+				message.channel.send(
+					`<@${message.author.id}>, cheesed to see you!`
+				);
+			}
 		}
 	},
 };
